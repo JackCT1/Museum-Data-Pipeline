9b02ea1198ca92cba85efe0a860c24d380b23e52
JackCT1 <jackthomas933@gmail.com>
2024-03-14 23:47:40 +0000

    create exhibitions table

--- pipeline/schema.sql
+++ pipeline/schema.sql
@@ -17,3 +17,19 @@ CREATE TABLE floors (
     floor_id INT PRIMARY KEY,
     floor_name VARCHAR(50) UNIQUE NOT NULL
 );
+
+CREATE TABLE exhibitions (
+    exhibition_id INT PRIMARY KEY,
+    exhibition_code VARCHAR(20),
+    exhibition_name VARCHAR(255) UNIQUE NOT NULL,
+    exhibition_start_date DATE NOT NULL,
+    exhibition_description TEXT NOT NULL,
+    department_id INT NOT NULL,
+    floor_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+            REFERENCES departments(department_id)
+            ON DELETE CASCADE,
+    FOREIGN KEY (floor_id)
+            REFERENCES floors(floor_id)
+            ON DELETE CASCADE,
+);
