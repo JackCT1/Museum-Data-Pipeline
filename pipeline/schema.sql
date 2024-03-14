82650fdc236f184ea1f6fcf73d8771d9193c2bd6
JackCT1 <jackthomas933@gmail.com>
2024-03-14 23:27:48 +0000

    creating departments and floors tables

--- pipeline/schema.sql
+++ pipeline/schema.sql
@@ -7,3 +7,13 @@ DROP TABLE IF EXISTS rating_event;
 DROP TABLE IF EXISTS support_event;
 DROP TABLE IF EXISTS rating_value;
 DROP TABLE IF EXISTS support_value;
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE floors (
+    floor_id INT PRIMARY KEY,
+    floor_name VARCHAR(50) UNIQUE NOT NULL
+);
