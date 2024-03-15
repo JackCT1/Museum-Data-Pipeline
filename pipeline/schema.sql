8ef53a0ce706d22abbe7033e0da88eebc26f0afb
JackCT1 <jackthomas933@gmail.com>
2024-03-15 14:50:24 +0000

    created rating events table

--- pipeline/schema.sql
+++ pipeline/schema.sql
@@ -3,33 +3,45 @@ DROP DATABASE If EXISTS museum;
 DROP TABLE IF EXISTS exhibitions CASCADE;
 DROP TABLE IF EXISTS floors CASCADE;
 DROP TABLE IF EXISTS departments CASCADE;
-DROP TABLE IF EXISTS rating_event;
-DROP TABLE IF EXISTS support_event;
-DROP TABLE IF EXISTS rating_value;
-DROP TABLE IF EXISTS support_value;
+DROP TABLE IF EXISTS rating_events CASCADE;
+DROP TABLE IF EXISTS support_events CASCADE;
+DROP TABLE IF EXISTS rating_values CASCADE;
+DROP TABLE IF EXISTS support_values CASCADE;
 
 CREATE TABLE departments (
-    department_id INT PRIMARY KEY,
-    department_name VARCHAR(255) UNIQUE NOT NULL
+    id INT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE floors (
-    floor_id INT PRIMARY KEY,
-    floor_name VARCHAR(50) UNIQUE NOT NULL
+    id INT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE exhibitions (
-    exhibition_id INT PRIMARY KEY,
-    exhibition_code VARCHAR(20),
-    exhibition_name VARCHAR(255) UNIQUE NOT NULL,
-    exhibition_start_date DATE NOT NULL,
-    exhibition_description TEXT NOT NULL,
+    id INT PRIMARY KEY,
+    code VARCHAR(20),
+    name VARCHAR(255) UNIQUE NOT NULL,
+    start_date DATE NOT NULL,
+    description TEXT NOT NULL,
     department_id INT NOT NULL,
     floor_id INT NOT NULL,
     FOREIGN KEY (department_id)
-            REFERENCES departments(department_id)
+            REFERENCES departments(id)
             ON DELETE CASCADE,
     FOREIGN KEY (floor_id)
-            REFERENCES floors(floor_id)
-            ON DELETE CASCADE,
+            REFERENCES floors(id)
+            ON DELETE CASCADE
 );
+
+CREATE TABLE rating_events ();
+
+CREATE TABLE rating_values (
+    id INT PRIMARY KEY,
+    value SMALLINT UNIQUE NOT NULL,
+    description TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE support_events ();
+
+CREATE TABLE support_values ();
